c74f4b54bb2bfcd18eaf314f279f1b652fc4ccc7
Anthony Lasserre <lasserre.anthony@gmail.com>
2025-05-26 01:33:26 +0200

    (fix) replace SQLite by PostgreSQL instead

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,12 @@ import { DataSource } from "typeorm";
 import { Chrono } from "../entities/Chrono";
 
 export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: "chronos.db",
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "chrono_db",
   synchronize: true,
   logging: true,
   entities: [Chrono],
